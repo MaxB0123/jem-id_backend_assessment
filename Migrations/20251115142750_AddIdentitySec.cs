8c0fc03a699d538921bc1277d69f6bc9011f50f6
MaxB0123 <22071628@student.hhs.nl>
2025-11-15 15:28:14 +0100

    Migrations update

---
+++ Migrations/20251115142750_AddIdentitySec.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jem_id_backend_assessment.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentitySec : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
